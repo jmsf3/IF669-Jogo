39704c78413a49c90ac0b7465e696f4c8a1a1bd4
João Marcelo <128498401+jmsf3@users.noreply.github.com>
2023-04-20 17:17:39 -0300

    Main atualizada

--- src/main.c
+++ src/main.c
@@ -5,10 +5,12 @@
 #include "./inimigos/inimigos.h"
 #include "./backgrounds/backgrounds.h"
 #include "../dep/include/raylib.h"
-#define LARG_JANELA 1000
-#define ALT_JANELA 600
-#define VEL_BACKGROUND 3
 #define FPS 60
+#define ESCALA 5
+#define ALT_JANELA 600
+#define LARG_JANELA 1000
+
+void UnloadTextures(Nave *nave, Inimigo *inimigo, ProjetilInimigo *projetil, int numInimigos, int numProjetil, Texture2D background);
 
 int main()
 {
@@ -18,19 +20,13 @@ int main()
     int frames = 0;
 
     int divisaoBackground = 0;
-    Texture2D backgroundEspaco = LoadTexture("../res/background_espaco.png");
+    Texture2D backgroundEspaco = LoadTexture("../res/backgrounds/background_espaco.png");
 
     Nave naveACM;
     inicializarNave(&naveACM);
-    
-    Propulsor *propulsor = NULL;
-    inicializarPropulsor(&propulsor);
-
-    int numProjetilNave = 0;
-    ProjetilNave *projetilNave = NULL;
 
     int numInimigos = 0;
-    Inimigo *inimigos = NULL;
+    Inimigo *inimigo = NULL;
 
     int numProjetilInimigo = 0;
     ProjetilInimigo *projetilInimigo = NULL;
@@ -38,49 +34,19 @@ int main()
     while (!WindowShouldClose())
     {   
         // Atualização
-        atualizarNave(&naveACM);
-        atualizarPropulsor(propulsor, naveACM, frames);
-        atualizarInimigos(&inimigos, &numInimigos, frames);
-        atualizarProjetilNave(naveACM, &projetilNave, &numProjetilNave);
-        atualizarProjetilInimigo(inimigos, numInimigos, naveACM, &projetilInimigo, &numProjetilInimigo, frames);
+        atualizarNave(&naveACM, frames);
+        atualizarBackground(&divisaoBackground);
+        atualizarInimigos(&inimigo, &numInimigos, frames);
+        atualizarProjetilInimigo(inimigo, numInimigos, naveACM, &projetilInimigo, &numProjetilInimigo, frames);
         
-        if (divisaoBackground > ALT_JANELA)
-            divisaoBackground = 0;
-        else
-            divisaoBackground += VEL_BACKGROUND;
-
         // Draw
         BeginDrawing();
 
-            // Background
             DrawScrollingBackground(backgroundEspaco, divisaoBackground); 
-            
-            // Inimigos
-            for (int i = 0; i < numInimigos; i++)
-            {
-                DrawTextureRec(inimigos[i].textura, inimigos[i].source, inimigos[i].posicao, WHITE); 
-            }
-
-            // Propulsores da nave
-            for (int i = 0; i < 2; i++)
-            {
-                DrawTextureRec(propulsor[i].textura, propulsor[i].source, propulsor[i].posicao, WHITE); 
-            }
-
-            // Nave
-            DrawTextureRec(naveACM.textura, naveACM.source, naveACM.posicao, WHITE);
-
-            // Projéteis da nave
-            for (int i = 0; i < numProjetilNave; i++)
-            {
-                DrawTextureV(projetilNave[i].textura, projetilNave[i].posicao, WHITE); 
-            }
-
-            // Projéteis do inimigo
-            for (int i = 0; i < numProjetilInimigo; i++)
-            {
-                DrawTextureV(projetilInimigo[i].textura, projetilInimigo[i].posicao, WHITE); 
-            }
+            DrawEnemy(inimigo, numInimigos);
+            DrawShip(naveACM);
+            DrawShipProjectile(naveACM);
+            DrawEnemyProjectile(projetilInimigo, numProjetilInimigo);
 
         EndDrawing();
 
@@ -88,20 +54,43 @@ int main()
     }
 
     // Dar unload nas texturas
-    UnloadTexture(naveACM.textura);
-    for (int i = 0; i < 2; i++) UnloadTexture(propulsor[i].textura);
-    for (int i = 0; i < numInimigos; i++) UnloadTexture(inimigos[i].textura);
-    for (int i = 0; i < numProjetilNave; i++) UnloadTexture(projetilNave[i].textura);
-    for (int i = 0; i < numProjetilInimigo; i++) UnloadTexture(projetilInimigo[i].textura);
-    UnloadTexture(backgroundEspaco);
+    UnloadTextures(&naveACM, inimigo, projetilInimigo, numInimigos, numProjetilInimigo, backgroundEspaco);
 
     // Liberar memória alocada
-    free(inimigos);
-    free(propulsor);
-    free(projetilNave);
+    free(inimigo);
     free(projetilInimigo);
+    free(naveACM.projetil);
 
     CloseWindow();
-
     return 0;
+}
+
+void UnloadTextures(Nave *nave, Inimigo *inimigo, ProjetilInimigo *projetil, int numInimigos, int numProjetil, Texture2D background)
+{
+    // Nave
+    UnloadTexture(nave->sprite);
+    for (int i = 0; i < 3; i++) UnloadTexture(nave->spritesheet[i]);
+    for (int i = 0; i < nave->numProjetil; i++) UnloadTexture(nave->projetil[i].sprite);
+
+    // Propulsores
+    for (int i = 0; i < 2; i++)
+    {
+        UnloadTexture(nave->polpulsor[i].sprite);
+
+        for (int j = 0; j < 4; j++) UnloadTexture(nave->polpulsor[i].spritesheet[j]);
+    }
+
+    // Inimigos
+    for (int i = 0; i < 2; i++)
+    {
+        UnloadTexture(inimigo[i].sprite);
+
+        for (int j = 0; j < 2; j++) UnloadTexture(inimigo[i].spritesheet[j]);
+    }
+
+    // Projéteis
+    for (int i = 0; i < numProjetil; i++) UnloadTexture(projetil[i].sprite);
+
+    // Background
+    UnloadTexture(background);
 }
